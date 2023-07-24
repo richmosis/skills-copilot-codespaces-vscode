4d882dd561c1e09970c3aa5fffca9665918612d6
richmosis <140354385+richmosis@users.noreply.github.com>
2023-07-24 00:03:47 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,38 @@
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Path: comments.js
+// Create web server
+app.use(express.static(path.join(__dirname, 'public')));
